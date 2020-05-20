d52879984ca6bc73fa93de9ed8586c22d847aac1
Edward <edward@Edwardde-MacBook-Pro.local>
2020-05-21 03:55:22 +0800

    first commit

---
+++ hw7_quantiztion.py
@@ -0,0 +1,59 @@
+import os
+import torch
+import numpy as np
+import pickle
+import sys
+
+print(f"\noriginal cost: {os.stat(sys.argv[1]).st_size} bytes.")
+params = torch.load(sys.argv[1])
+
+def encode8(params, fname):
+    custom_dict = {}
+    for (name, param) in params.items():
+        param = np.float64(param.cpu().numpy())
+        if type(param) == np.ndarray:
+            min_val = np.min(param)
+            max_val = np.max(param)
+            param = np.round((param - min_val) / (max_val - min_val) * 255)
+            param = np.uint8(param)
+            custom_dict[name] = (min_val, max_val, param)
+        else:
+            custom_dict[name] = param
+
+    pickle.dump(custom_dict, open(fname, 'wb'))
+
+
+def decode8(fname):
+    params = pickle.load(open(fname, 'rb'))
+    custom_dict = {}
+    for (name, param) in params.items():
+        if type(param) == tuple:
+            min_val, max_val, param = param
+            param = np.float32(param)
+            param = (param / 255 * (max_val - min_val)) + min_val
+            param = torch.tensor(param)
+        else:
+            param = torch.tensor(param)
+
+        custom_dict[name] = param
+
+    return custom_dict
+
+encode8(params, '8_bit_model.pkl')
+print(f"8-bit cost: {os.stat('8_bit_model.pkl').st_size} bytes.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
